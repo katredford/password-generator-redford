264e367473be99a8b29d89472330cdf3dc4d0d6a
Katie Redford <katier.@KRedfords-Mini.attlocal.net>
2021-03-16 10:47:28 -0500

    add arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y"]
+var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+// var nums = ["1","2","3","4","5","6","7","8","9","0",];
 
-var wantLower =confirm("include lower case letters?")
+// var wantLower =confirm("include lower case letters?")
 
 //confirm('U want upper letters ??')
 
@@ -14,11 +16,11 @@ var wantLower =confirm("include lower case letters?")
   
 // }
 
-var promptAsk = window.prompt("How many characters do you want in your password?");
+// var promptAsk = window.prompt("How many characters do you want in your password?");
 
-if (promptAsk <= 8 || promptAsk >= 128) {
-  console.log(promptAsk)
-}
+// if (promptAsk <= 8 || promptAsk >= 128) {
+//   var chatlength
+// }
 
 
 // Write password to the #password input
@@ -40,7 +42,7 @@ function generatePassword() {
   for (let index = 0; index < lower.length; index++) {
     console.log('TIme to make password!!', lower[Math.floor(Math.random() * lower.length)]);
     
-    finalPassword += lower[Math.floor(Math.random() * lower.length)]; +
+    finalPassword += lower[Math.floor(Math.random() * lower.length)]; 
   
   }
 
