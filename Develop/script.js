b9cdf0d9ac67dfe718ef7d3796f38a8ab126c69b
Katie Redford <katier.@KRedfords-Mini.attlocal.net>
2021-03-16 10:13:01 -0500

    add lower case array

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lower = ['a','b','c','d','e','f']
+var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y"]
 
 var wantLower =confirm("include lower case letters?")
 
 //confirm('U want upper letters ??')
 
-if(wantLower) {
-  alert("lowercase plez!")
-}
-else {
-  alert("nothnx")
-}
-
-// var promptAsk = window.prompt("How many characters do you want in your password?");
-
-// if (promptAsk <= 8 || promptAsk >= 128) {
-//   console.log(promptAsk)
+// if(wantLower) {
+//   alert("lowercase plez!")
 // }
+// else {
+  
+// }
+
+var promptAsk = window.prompt("How many characters do you want in your password?");
+
+if (promptAsk <= 8 || promptAsk >= 128) {
+  console.log(promptAsk)
+}
 
 
 // Write password to the #password input
@@ -40,7 +40,7 @@ function generatePassword() {
   for (let index = 0; index < lower.length; index++) {
     console.log('TIme to make password!!', lower[Math.floor(Math.random() * lower.length)]);
     
-    finalPassword += lower[Math.floor(Math.random() * lower.length)]; 
+    finalPassword += lower[Math.floor(Math.random() * lower.length)]; +
   
   }
 
