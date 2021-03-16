ffe15fc217d10d6e2e861b63708cef49786270ac
Katie Redford <katier.@KRedfords-Mini.attlocal.net>
2021-03-16 12:52:44 -0500

    figured out password length via user input

--- Develop/script.js
+++ Develop/script.js
@@ -2,25 +2,28 @@
 var generateBtn = document.querySelector("#generate");
 
 var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-// var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-// var nums = ["1","2","3","4","5","6","7","8","9","0",];
+var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var nums = ["1","2","3","4","5","6","7","8","9","0",];
+var chars = ["!","@","#","$","%","^","&","*","(",")","=","+",];
 
-// var wantLower =confirm("include lower case letters?")
+var wantLower =confirm("include lower case letters?")
 
-//confirm('U want upper letters ??')
+// confirm('U want upper letters ??')
 
-// if(wantLower) {
-//   alert("lowercase plez!")
-// }
-// else {
+if(wantLower) {
+  console.log(lower)
+}
+else {
   
-// }
+}
 
-// var promptAsk = window.prompt("How many characters do you want in your password?");
+var chatLength = window.prompt("How many characters do you want in your password?");
 
-// if (promptAsk <= 8 || promptAsk >= 128) {
-//   var chatlength
-// }
+if (chatLength >=8 && chatLength <= 128) {
+  console.log(chatLength);
+} else {
+  alert("Must be between 8 and 128 characters.");
+}
 
 
 // Write password to the #password input
